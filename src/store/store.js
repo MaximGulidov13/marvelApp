51060870e5d3f2d0af009c4c3d425924544ba80b
Maxim <akuakuaku1304@gmail.com>
2023-12-31 14:20:35 +0600

    starting

---
+++ src/store/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const stringMiddleware = (store) => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
+    }
+    return next(action)
+}
+
+const store = configureStore({
+
+})
